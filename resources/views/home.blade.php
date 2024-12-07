ef6799064f61ce01455c69e01fe17456035cb86e
abusayed248 <abusayed20012022@gmail.com>
2024-12-07 21:37:57 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,253 @@
+<x-app-layout>
+
+    <div class="d-flex mt-5">
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center flex-column align-items-center">
+            <h3 class="h3">Greeting for Sub-menu</h3>
+            <div class="d-flex justify-content-center align-items-center">
+            <div class="col-md-1">
+              <label id="file-name"  for="file-input"> File</label>
+            </div>
+            <div class="col-md-11">
+              <div class="custom-file mx-3">
+                <label id="file-name" class="file-label play" for="file-input">choose File</label>
+                <input type="file" id="file-input" class="file-input" />
+              </div>
+            </div>
+            
+              
+            </div>
+            <a class="text-decoration" href="">Replace</a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">Play MP3</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+         
+              <label id="file-name"  for="file-input">File</label>
+          
+              <div class="custom-file l-magin">
+                <label id="file-name" class="file-label play" for="file-input">choose File</label>
+                <input type="file" id="file-input" class="file-input" />
+              </div>
+          
+            
+              
+            </div>
+              <a class="l-magin text-decoration" href="">Replace</a>
+          </div>
+        
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">Play MP3</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+         
+              <label id="file-name"  for="file-input">File</label>
+          
+              <div class="custom-file l-magin">
+                <label id="file-name" class="file-label play" for="file-input">choose File</label>
+                <input type="file" id="file-input" class="file-input" />
+              </div>
+          
+            
+              
+            </div>
+              <a class="l-magin text-decoration" href="">Replace</a>
+          </div>
+        
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">Play MP3</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+         
+              <label id="file-name"  for="file-input">File</label>
+          
+              <div class="custom-file l-magin">
+                <label id="file-name" class="file-label play" for="file-input">choose File</label>
+                <input type="file" id="file-input" class="file-input" />
+              </div>
+          
+            
+              
+            </div>
+              <a class="l-magin text-decoration" href="">Replace</a>
+          </div>
+        
+        </div>
+      
+      </div>
+      <div class="d-flex mt-5">
+        <div class="col-md-3">
+         
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">Play MP3</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+         
+              <label id="file-name"  for="file-input">File</label>
+          
+              <div class="custom-file l-magin">
+                <label id="file-name" class="file-label play" for="file-input">choose File</label>
+                <input type="file" id="file-input" class="file-input" />
+              </div>
+          
+            
+              
+            </div>
+              <a class="l-magin text-decoration" href="">Replace</a>
+          </div>
+        
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">None</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+          </div>
+        
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">None</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+       
+          </div>
+        
+        </div>
+      
+      </div>
+      <div class="d-flex mt-5">
+        <div class="col-md-3 d-flex justify-content-center ">
+         <h3><a class="text-center large-text text-decoration" href="">Manage Subscribers</a></h3>
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">None</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+          </div>
+        
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">None</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+          </div>
+        
+        </div>
+        <div class="col-md-3">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+           <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+          
+              <label for="">Function</label>
+       
+          
+              <select name="cars" id="cars" form="" class="play">
+                <option value="volvo">None</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+           
+           </div>
+       
+          </div>
+        
+        </div>
+      
+      </div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+        <link href="{{ asset('custom-files/style.css') }}" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div>
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        <script>
+            const fileInput = document.getElementById('file-input');
+            const fileName = document.getElementById('file-name');
+      
+            fileInput.addEventListener('change', (event) => {
+              const file = event.target.files[0];
+              fileName.textContent = file ? file.name : 'No file chosen';
+            });
+      
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/js/all.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+        <script src="{{ asset('custom-files/main.js') }}" type="text/javascript" />
+    </body>
+</html>
