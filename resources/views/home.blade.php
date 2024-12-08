1a2c72926df91bfd4893e1b5094510bb2f77328a
abusayed248 <abusayed20012022@gmail.com>
2024-12-08 12:09:38 +0600

    main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,23 @@
                   <input type="file" id="file-input" class="file-input" />
                 </div>
               </div>
-              
+                
+              </div>
+              <a id="replace-file-text" class="text-decoration  no-style" href="javascript:void(0)">Replace</a>
+              <input id="replace-file-input" type="file" style="display:none" class="replace-img" />
+            </div>
+            <div class="d-flex mt-5 justify-content-center flex-column align-items-center">
+              <h3 class="h3">Greeting for non-subscribers</h3>
+              <div class="d-flex justify-content-center align-items-center">
+              <div class="col-md-1">
+                <label id="file-name"  for="file-input"> File</label>
+              </div>
+              <div class="col-md-11">
+                <div class="custom-file mx-3">
+                  <label id="file-name" class="file-label play" for="file-input">choose File</label>
+                  <input type="file" id="file-input" class="file-input" />
+                </div>
+              </div>
                 
               </div>
               <a id="replace-file-text" class="text-decoration  no-style" href="javascript:void(0)">Replace</a>
@@ -33,33 +49,30 @@
             <div class="row">
               <div class="col-md-4">
                 <div class="d-flex justify-content-center align-items-center flex-column">
-                <div class="d-flex justify-content-center align-items-center mt-2 w-100">
-                
-                    <label for="">Function</label>
-            
-                
-                    <select name="cars" id="cars" form="" class="play">
-                      <option value="volvo">Play MP3</option>
-                      <option value="saab">Saab</option>
-                      <option value="opel">Opel</option>
-                      <option value="audi">Audi</option>
-                    </select>
-                
-                </div>
-                <div class="d-flex justify-content-center align-items-center mt-2 w-100">
-              
-                    <label id="file-name"  for="file-input">File</label>
-                
-                    <div class="custom-file l-magin">
-                      <label id="file-name" class="file-label play" for="file-input">choose File</label>
-                      <input type="file" id="file-input" class="file-input" />
-                    </div>
-                
+                  <div class="d-flex justify-content-center align-items-center mt-2 w-100">
                   
-                    
+                      <label for="">Function</label>
+              
+                      <select name="cars" id="cars" form="" class="play">
+                        <option value="volvo">Transfer</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                      </select>
+                  
+                  </div>
+                  
+                  <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+                      <label for="">To</label>
+                      <input type="text" class="w-100 " placeholder="+1123355656" />
                   </div>
                   <a id="replace-file-text" class="text-decoration" href="javascript:void(0)">Replace</a>
                   <input id="replace-file-input" type="file" style="display:none" class="replace-img" />
+
+                  <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+                    <label for="">Enter afer (minutes)</label>
+                    <input type="number" class="w-100 " placeholder="60" />
+                  </div>
                 </div>
               
               </div>
@@ -68,9 +81,8 @@
                 <div class="d-flex justify-content-center align-items-center mt-2 w-100">
                 
                     <label for="">Function</label>
-            
                 
-                    <select name="cars" id="cars" form="" class="play">
+                    <select name="cars" id="cars" class="play">
                       <option value="volvo">Play MP3</option>
                       <option value="saab">Saab</option>
                       <option value="opel">Opel</option>
@@ -87,8 +99,6 @@
                       <input type="file" id="file-input" class="file-input" />
                     </div>
                 
-                  
-                    
                   </div>
                   <a id="replace-file-text" class="text-decoration" href="javascript:void(0)">Replace</a>
                   <input id="replace-file-input" type="file" style="display:none" class="replace-img" />
@@ -223,29 +233,43 @@
               
                 
                     <select name="cars" id="cars" form="" class="play">
-                      <option value="volvo">None</option>
-                      <option value="saab">Saab</option>
-                      <option value="opel">Opel</option>
-                      <option value="audi">Audi</option>
+                      <option value="Play MP3">Play MP3</option>
+                      <option value="Transfer">Transfer</option>
+                      <option value="Submenu">Submenu</option>
                     </select>
                   
                   </div>
                 </div>
               
               </div>
-              <div class="col-md-4 mt-5 ">
+              <div class="col-md-4">
                 <div class="d-flex justify-content-center align-items-center flex-column">
                   <div class="d-flex justify-content-center align-items-center mt-2 w-100">
-                
-                    <label for="">Function</label>
-                    <select name="cars" id="cars" form="" class="play">
-                      <option value="volvo">None</option>
-                      <option value="saab">Saab</option>
-                      <option value="opel">Opel</option>
-                      <option value="audi">Audi</option>
-                    </select>
+                  
+                      <label for="">Function</label>
+              
+                      <select name="cars" id="cars" form="" class="play">
+                        <option value="volvo">Transfer</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                      </select>
+                  
+                  </div>
+                  
+                  <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+                      <label for="">To</label>
+                      <input type="text" class="w-100 " placeholder="+1123355656" />
+                  </div>
+                  <a id="replace-file-text" class="text-decoration" href="javascript:void(0)">Replace</a>
+                  <input id="replace-file-input" type="file" style="display:none" class="replace-img" />
+
+                  <div class="d-flex justify-content-center align-items-center mt-2 w-100">
+                    <label for="">Enter afer (minutes)</label>
+                    <input type="number" class="w-100 " placeholder="60" />
                   </div>
                 </div>
+              
               </div>
             </div>
           </div>
