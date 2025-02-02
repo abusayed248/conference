28aa909eaadd87ff67306e05c3ccbfc2228f73aa
Saidurbu <mdsaidurbu97@gmail.com>
2025-02-02 17:30:09 +0600

    check

---
+++ app/Services/SubscriptionsService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use App\Models\User;
+
+
+
+class SubscriptionsService
+{
+
+    public function isActive($phoneNumber)
+    {
+        $user = User::query()->where('number', $phoneNumber)->first();
+
+        if ($user && $user->payment_done && $user->payment_end && Carbon::now()->lt($user->payment_end)) {
+            return true; // Active subscription
+        }
+
+        return false; // Inactive subscription or user not found
+    }
+}
